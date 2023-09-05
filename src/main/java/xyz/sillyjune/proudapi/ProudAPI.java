88146246505909c6e89ef125cca4d1eea61967eb
Juniper <86971433+JuneDeveloper@users.noreply.github.com>
2023-09-05 19:16:18 +0100

    Initial commit

---
+++ src/main/java/xyz/sillyjune/proudapi/ProudAPI.java
@@ -0,0 +1,13 @@
+package xyz.sillyjune.proudapi;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ProudAPI implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/xyz/sillyjune/proudapi/ProudAPIClient.java
@@ -0,0 +1,13 @@
+package xyz.sillyjune.proudapi;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ProudAPIClient implements ClientModInitializer {
+    /**
+     * Runs the mod initializer on the client environment.
+     */
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
